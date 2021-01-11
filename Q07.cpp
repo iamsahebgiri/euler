d90f051109cdc7b3e5764a801aefd44fba9ec42e
iamsahebgiri <iamsahebgiri@gmail.com>
2021-01-11 16:31:54 +0530

    style(Q07): code format change

--- Q07.cpp
+++ Q07.cpp
@@ -1,30 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define deb(x) cout<<#x<<" "<<x<<'\n'
+#define deb(x) cout << #x << " " << x << '\n'
 
 int main()
 {
     long long t = 1;
     int n = 2;
-    while(t<10002)
+    while (t < 10002)
     {
-        
+
         bool flag = false;
         for (int i = 2; i < n; i++)
         {
-            if(n%i == 0)
+            if (n % i == 0)
             {
-                flag  = true;
+                flag = true;
                 break;
             }
         }
-        if(!flag)
+        if (!flag)
         {
-            cout<<t<<": "<<n<<endl;
+            cout << t << ": " << n << endl;
             t++;
         }
         n++;
     }
-    
+
     return 0;
 }
